b2a57ed19e4bda3628c37771d5d790ad60f02a08
Josue Tapia <josue.tapia.comp@gmail.com>
2021-05-20 15:49:35 -0500

    funcionalidad de busqueda con auntocompletado

--- src/Pelicula.js
+++ src/Pelicula.js
@@ -4,18 +4,21 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme)=>({
   root: {
-   width:'20%',
+   width:'300px',
    margin: 10,
    padding:10,
    position:'relative',
-  },
-  title: {
-    fontSize: 20,
+   transition: theme.transitions.create('box-shadow'),
+   '&:hover': {
+    backgroundColor: '#d3d3d3',
+    cursor:'pointer',
+    border: '2px solid black',
+    boxShadow: '2px 2px 2px 2px #333'
+    }
   },
   description: {
-    fontSize: 14,
     textAlign:'justify'
   },
   director:{
@@ -31,15 +34,42 @@ const useStyles = makeStyles({
     bottom:0,
     right:0,
     color:'blue'
+  },
+  running_time:{
+    margin:5
   }
-})
+}))
 export default function Pelicula(props) {
   const classes = useStyles();
 
+  const timeCalculator = (minutes)=>{
+    let time = '';
+    const getHoursAndMinutes = minutes =>{
+      const hours = ~~(minutes/60)
+      minutes = minutes%60
+      return `${hours} hr. ${minutes} min.`
+    }
+    const typeOfMinutes = typeof minutes;
+    switch (typeOfMinutes) {
+      case 'number':
+        time = getHoursAndMinutes(minutes);
+        break;
+      case 'string':
+        minutes = parseInt(minutes);
+        time = getHoursAndMinutes(minutes);
+        break;
+    
+      default:
+        time = "0 hr. 0 min."
+        break;
+    }
+    return time;
+  }
+
   return (
-      <Card className={classes.root}>
+        <Card className={classes.root}>
       <CardContent>
-        <Typography className={classes.title} >
+        <Typography >
         {props.title}
         </Typography>
         <Typography 
@@ -55,6 +85,10 @@ export default function Pelicula(props) {
         <Typography className={classes.release_date} 
         component='p'>
         {props.release_date}
+        </Typography>
+        <Typography className={classes.running_time}
+        component='p'>
+        Running time: {timeCalculator(props.running_time)}
         </Typography>
       </CardContent>
     </Card>
